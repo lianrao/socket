66d254d23591c64407b5493f42b0e0c50a5a127d
rubyji <rubyji@hxfai.com>
2020-11-19 18:12:40 +0800

    client.py

---
+++ client.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 16 02:46:16 2020
+
+@author: jiyut
+python3
+"""
+import socket
+import sys
+
+import clientHandler as ch
+serverAddr = "127.0.0.1"
+serverPort = 12345
+
+#serverAddr = str(sys.argv[2])
+#serverPort = int(sys.argv[3])
+#import clientService as cs
+
+
+
+
+
+def main():
+    print(">python Client %s %d"% (serverAddr, serverPort))
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((serverAddr, serverPort))
+        username = input("Enter username: ")
+        s.send(("un/"+username).encode())
+        while True:
+            msg = s.recv(1024).decode('utf-8')
+            #msg = cs.main(msg)
+            #print(msg)
+            s.send(msg.encode('utf-8'))
+
+
+
+if __name__ == "__main__":
+    main()
